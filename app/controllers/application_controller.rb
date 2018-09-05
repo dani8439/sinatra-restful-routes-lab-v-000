9a3cddc67a67e69043bb9fc714cc8830c2b68714
Dani Schuhman <dani8439@aol.com>
2018-09-05 18:51:45 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
 
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
-    erb :index
+    erb :show
   end
 
   get '/recipes/:id/edit' do
