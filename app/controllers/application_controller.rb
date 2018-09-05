768082df46b45fc3bc278037d9f2b38577f92ce9
Dani Schuhman <dani8439@aol.com>
2018-09-05 18:38:01 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,8 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
-  post '/recipes' do 
-    @recipe = Recipe.create(:title => params[:title], :)
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :)
   end
 
   get '/recipes/:id' do
